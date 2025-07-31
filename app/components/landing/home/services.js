43760f2668d3f53cf89e6499c66ba4ff908aa699
MD AZHARUDDIN <azhar73397@gmail.com>
2025-08-01 02:23:33 +0600

    Services added

--- app/components/landing/home/services.js
+++ app/components/landing/home/services.js
@@ -1,30 +1,36 @@
 "use client";
+import Image from "next/image";
 
 const services = [
   {
     title: "Building Design Plans",
     description:
       "We at ABCON ensure that every design is convened to the best level both aesthetically and conventionally to our client’s preferences.",
+    image: "/service/s1.webp",
   },
   {
     title: "Civil Engineering",
     description:
       "Our team of Engineers guarantee cost effective and approvable designs for all projects. Click on the below to find out more information",
+    image: "/service/s2.webp",
   },
   {
     title: "Structural Engineering",
     description:
       "We have a holistic approach to Structural Design. An iterative method is employed in delivering functional, easy-to-build and feasible designs.",
+    image: "/service/s3.webp",
   },
   {
     title: "Survey and Site Analysis",
     description:
       "At ABCON we can organise a survey of your property to ensure an accurate design. To find out more information, give us a call or click here.",
+    image: "/service/s4.png",
   },
   {
     title: "Certifications and Approvals",
     description:
       "We offer full engineering certification of all proposed and existing structural works. Click on the below to find out more information",
+    image: "/service/s5.webp",
   },
 ];
 
@@ -47,18 +53,38 @@ export default function ServicesSection() {
               key={index}
               className="bg-white p-6 rounded shadow hover:shadow-lg transition duration-300 border-l-4 border-blue-900"
             >
-              <h3 className="text-xl font-semibold text-blue-900 mb-3">{service.title}</h3>
-              <p className="text-gray-700">{service.description}</p>
+              <div className="mb-4">
+                <Image
+                  src={service.image}
+                  alt={service.title}
+                  width={80}
+                  height={80}
+                  className="object-contain mx-auto"
+                />
+              </div>
+              <h3 className="text-xl font-semibold text-blue-900 mb-3 text-center">{service.title}</h3>
+              <p className="text-gray-700 text-center">{service.description}</p>
             </div>
           ))}
+          <div
+              
+              className="bg-white p-6 flex flex-col justify-center items-center rounded shadow bg-gradient-to-r from-[#1C72B5] to-[#1E317E] 
+             bg-[length:200%_200%] transition-all duration-500 ease-in-out
+             hover:animate-gradient-x hover:shadow-lg transition duration-300 border-l-4 border-blue-900"
+            >
+              <div className="mb-4 flex items-center justify-center">
+                <Image
+                  src= "/service/s6.png"
+                  alt="call"
+                  width={60}
+                  height={60}
+                  className="object-contain mx-auto"
+                />
+              </div>
+              <h3 className="text-xl font-semibold text-[#91B276] mb-3 text-center">Contact Us</h3>
+            </div>
         </div>
-
-        {/* Contact Button */}
-        <div className="mt-12 text-center">
-          <button className="bg-blue-900 text-white px-8 py-3 rounded hover:bg-blue-700 transition">
-            Contact Us
-          </button>
-        </div>
+        
       </div>
     </section>
   );
